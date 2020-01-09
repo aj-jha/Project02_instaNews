c927d2ee4741f2bd00ad68fb88b174e41a52108d
aj-jha <abhishek.jha.06@gmail.com>
2020-01-09 16:01:49 -0500

    feature: API added successfully

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 $(document).ready(function() {
   console.log("ready!");
 });
+
+//Note it should be on change, not click!
+$("#sectionSelection").on("change", function() {
+  $(".container").empty();
+  //console.log("changed"); //Notes: sanity check, look at console log to see if it worked
+  let sectionSelected = $("#sectionSelection :selected").val();
+  // console.log(sectionSelected);
+  let sectionURL =
+    "https://api.nytimes.com/svc/topstories/v2/" + sectionSelected + ".json";
+  // console.log(sectionURL);
+  $.ajax({
+    method: "GET",
+    url: sectionURL,
+    data: {
+      "api-key": "whSPI6fGaH2iGcOc68SgTPGTe0YPwVTM"
+    }
+  }).done(function(data) {
+    console.log(data);
+    // $(".container").each(function(data) {
+    //   $(this).append(`<p>${results.abstract}</p>`);
+    // });
+    let imageCounter = 0;
+
+    for (let n = imageCounter; n <= 12; n++) {
+      if (data.results[n].multimedia.length > 1) {
+        $(".container").append(`
+        <div class="gridCell">
+          <a href=${data.results[n].short_url}>
+            <img class="storyImage" src=${data.results[n].multimedia[4].url}>
+          </a>
+          <p class="gridText">${data.results[n].abstract}</p>
+        </div>`);
+        imageCounter++;
+      }
+    }
+  });
+});
